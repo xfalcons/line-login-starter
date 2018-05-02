cc5ccb546d12066684aec1b5e11d27d27808cd61
Kevin Luo <kevin.luo@linecorp.com>
2018-05-02 12:00:07 +0800

    Modify api to beta

--- src/main/java/com/linecorp/sample/login/infra/line/api/v2/LineAPIService.java
+++ src/main/java/com/linecorp/sample/login/infra/line/api/v2/LineAPIService.java
@@ -108,7 +108,7 @@ public class LineAPIService {
             throw new RuntimeException(e);
         }
 
-        return "https://access.line.me/oauth2/v2.1/authorize?response_type=code"
+        return "https://access.line-beta.me/oauth2/v2.1/authorize?response_type=code"
                 + "&client_id=" + channelId
                 + "&redirect_uri=" + encodedCallbackUrl
                 + "&state=" + state
@@ -120,7 +120,7 @@ public class LineAPIService {
         try {
             JWT.require(
                 Algorithm.HMAC256(channelSecret))
-                .withIssuer("https://access.line.me")
+                .withIssuer("https://access.line-beta.me")
                 .withAudience(channelId)
                 .withClaim("nonce", nonce)
                 .build()
@@ -136,7 +136,7 @@ public class LineAPIService {
     }
 
     private <R> R getClient(final Function<LineAPI, Call<R>> function) {
-        return Client.getClient("https://api.line.me/", LineAPI.class, function);
+        return Client.getClient("https://api.line-beta.me/", LineAPI.class, function);
     }
 
 }
